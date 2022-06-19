24c16cd16e2e034dc9d59f58f0c282ee902f911c
Pedro Lourenco <pl156176@gmail.com>
2022-06-19 17:01:06 +0100

    Created home_page.dart. This page acts as a container for other pages by handing bottomNavigationBar naviation

---
+++ lib/home_page/home_page.dart
@@ -0,0 +1,63 @@
+import 'package:akioo_mobile/photo_feed/photo_feed_page.dart';
+import 'package:akioo_mobile/profile/profile_page.dart';
+import 'package:akioo_mobile/scan_item/scan_item_page.dart';
+import 'package:akioo_mobile/collections/collections_page.dart';
+import 'package:flutter/material.dart';
+
+//Page which contains bottom navigation bar
+//Other pages are loaded on top of this page through the body argument of the Scaffold widget
+//All pages are stored in _pageOptions array
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedPage = 0;
+
+  final _pageOptions = const [
+    PhotoFeedPage(),
+    ScanItemPage(),
+    CollectionsPage(),
+    ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: _pageOptions[selectedPage],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.qr_code_scanner),
+              label: 'Scan',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.checkroom),
+              label: 'Collections',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: selectedPage,
+          onTap: (index) {
+            setState(() {
+              selectedPage = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
