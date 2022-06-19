0b785251be35ea04bf8007ab2276898a57a4f111
Pedro Lourenco <pl156176@gmail.com>
2022-06-19 17:03:33 +0100

    Created themes.dart file which contains app styles

---
+++ lib/themes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static ThemeData globalTheme = ThemeData.dark().copyWith(
+    brightness: Brightness.light,
+    backgroundColor: Colors.black,
+    scaffoldBackgroundColor: Colors.black,
+    canvasColor: Colors.black,
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+    textTheme: const TextTheme(
+      headline1: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold, fontFamily: 'LibreBodoni'),
+      headline2: TextStyle(fontSize: 36.0),
+    )
+  );
+}
