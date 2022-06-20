e02ec9a639e640fc5ef5116a19d55a230a3c69b5
Pedro Lourenco <pl156176@gmail.com>
2022-06-20 22:25:29 +0100

    add basic profile container

--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:akio_mobile/device_info.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -12,16 +13,53 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Center(
+        Container(
+          padding: EdgeInsets.only(
+            top: DeviceInfo.deviceHeight(context) * 0.01,
+          ),
+          alignment: Alignment.topLeft,
           child: Text(
             'Profile',
             style: Theme.of(context).textTheme.headline1,
+            textAlign: TextAlign.left,
           ),
         ),
         Container(
-          margin: EdgeInsets.only(top: 10),
-          height: 40,
-          decoration: const BoxDecoration(color: Colors.white),
+          margin: EdgeInsets.only(
+            top: DeviceInfo.deviceHeight(context) * 0.25,
+          ),
+          height: DeviceInfo.deviceHeight(context) * 0.55,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding:
+                        EdgeInsets.all(DeviceInfo.deviceWidth(context) * 0.15),
+                    child: const Text(
+                      'Name',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25.0,
+                          fontFamily: 'LibreBodoni'),
+                    ),
+                  ),
+                  Icon(
+                    Icons.account_circle,
+                    color: Colors.black,
+                    size: DeviceInfo.deviceHeight(context) * 0.12,
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ],
     );
