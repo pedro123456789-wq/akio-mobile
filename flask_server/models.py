b6c4d44ac4bb78aed23d5dd00cff4c03efc2f54c
Taran <fondcoolman@outlook.com>
2022-06-23 11:30:52 +0100

    Database models progress

--- flask_server/models.py
+++ flask_server/models.py
@@ -1,42 +1,74 @@
 # TODO: FINISH ME
+#       Relationships,
+#       Cascade behaviour
 
 # Images will refer to the name of the image, assigned via uuid, in the filesystem
+# Using String(100) since i doubt usernames, sizes, passwords, etc will ever exceed 100 characters, and postgresql requires a length to be provided with String
 
 class User(Model):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    username = Column(String(100), unique=True, nullable=False)
+    hashed_password = Column(String(100), nullable=False)
 
+    # For avatar
+    clothing_id = Column(Integer, ForeignKey("ClothingVariant.uuid"))
+    background_colour = Column(String(100), nullable=False)
+
+    # Posts
+    liked_posts = relationship("Like", back_populates="user")
+
+    # Clothes
+    owned_clothes = relationship("ClothingItem", back_populates="user")
 
 class ClothingVariant(Model):
     __tablename__ = "clothing_variant"
-    uuid = Column(String, primary_key=True)  # Can be used to fetch the image from filesystem
+    uuid = Column(String(100), primary_key=True)  # Can be used to fetch the image from filesystem
     # Using as primary key will take up a bit more space in memory maybe, but means that any uuid checks can be completed instantly due to indexing (i think)
 
-    size_id = Column(Integer, ForeignKey('size.id'), nullable=False)
-    colour_id = Column(Integer, ForeignKey('colour.id'), nullable=False)
+    name = Column(String(100), nullable=False)
+
+    size_id = Column(Integer, ForeignKey("Size.id"), nullable=False)
+    colour_id = Column(Integer, ForeignKey("Colour.id"), nullable=False)
+
+    owners = relationship("ClothingItem", back_populates="variant")
 
 
 class Size(Model):
     __tablename__ = "size"
     id = Column(Integer, primary_key=True)
-    info = Column(String, nullable=False, unique=True)
-
-    # todo: define relationship to get all clothingvariants of a specified colour
+    size = Column(String(100), nullable=False, unique=True)
 
 
 class Colour(Model):
     __tablename__ = "colour"
     id = Column(Integer, primary_key=True)
-    colour = Column(String, nullable=False, unique=True)
-
-    # todo: define relationship to get all clothingvariants of a specified size
+    colour = Column(String(100), nullable=False, unique=True)
 
 
-# Many to many relationship middle table
-class AvatarInfo(db.Model):
-    clothing_id = Column(Integer, ForeignKey('clothingvariant.uuid'))
-    user_id = Column(Integer, ForeignKey('user.id'))
+class Post(Model):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True) # Don't use uuid, because the association table Like, will repeat a long uuid lots of times in memory which is probs not good
+    uuid = Column(String(100), nullable=False)  # Used for accessing image file
+    date_posted = Column(DateTime, nullable=False)
+    poster_id = Column(Integer, ForeignKey("User.id"))
 
-    # todo: relationship
+    liked_by = relationship("Like", back_populates="post")
+
+
+class Like(Model):
+    __tablename__ = "like"
+    user_id = Column(Integer, ForeignKey("User.id"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("Post.uuid"), primary_key=True)
+
+    user = relationship("User", back_populates="liked_posts")
+    post = relationship("Post", back_populates="liked_by")
+
+
+class ClothingItem(Model):
+    __tablename__ = "clothing_item"
+    user_id = Column(Integer, ForeignKey("User.id"), primary_key=True)
+    variant_id = Column(Integer, ForeignKey("ClothingVariant.uuid"), primary_key=True)
+
+    user = relationship("User", back_populates="owned_clothes")
+    variant = relationship("ClothingVariant", back_populates="owners")
