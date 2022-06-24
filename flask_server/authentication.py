3b1e7e8591c9eabbced04e455a4e4cb6d42427bb
Pedro Lourenco <pl156176@gmail.com>
2022-06-24 22:51:29 +0100

    Create decorator to check if user is logged in

--- flask_server/authentication.py
+++ flask_server/authentication.py
@@ -1,38 +1,40 @@
 from functools import wraps
 import jwt
 from datetime import datetime
-from jsonschema import validate, exceptions
+from pydantic import ValidationError
 
-
-from flask_server.validation_schemes import sessionValidationSchema
+from flask_server import app
+from flask_server.responses import customResponse
+from flask_server.validation_schemas import SessionValidation
 from flask_server.models import User
 from flask import request
 
 
-
-def loginRequired(methods = None):
-    if methods == None: 
+def loginRequired(methods=None):
+    if methods == None:
         methods = ['GET', 'PUT', 'POST']
 
     def wrapper(function):
         @wraps(function)
         def decorated(*args, **kwargs):
             if request.method in methods:
-                # for get requests data will be sent in headers
+                # for GET requests data will be sent in headers but for POST and PUT requests it will be sent as json in request body
                 if request.method == 'GET':
                     data = request.headers
                 else:
                     data = request.get_json()
 
-                #check if headers are valid with json schema
+                # check if headers are valid with json schema
                 try:
-                    validate(data, schema = sessionValidationSchema)
-                except exceptions.ValidationError as error:
-                    return customResponse(False, error.message)
+                    SessionValidation(**data)
+                except ValidationError as error:
+                    topError = error[0]
+                    errorString = f"{topError['message']} for {topError['loc']}"
+                    return customResponse(False, errorString)
 
                 username, token = data.get('username'), data.get('token')
 
-                #check if token is valid and has not expired
+                # check if token is valid and has not expired
                 try:
                     decodedToken = jwt.decode(token, app.config['SECRET_KEY'])
                     tokenUsername, tokenExpiration = decodedToken.get('user'), decodedToken.get('exp')
@@ -45,9 +47,5 @@ def loginRequired(methods = None):
                     return customResponse(False, 'Invalid token')
 
             return function(*args, **kwargs)
-        return decorated    
+        return decorated
     return wrapper
-
-
-
-
