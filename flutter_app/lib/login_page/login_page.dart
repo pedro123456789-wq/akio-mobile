de792cb62a082596594557e3129cb02d98586a2f
Tazmondo <fondcoolman@outlook.com>
2022-07-03 10:20:40 +0100

    Make login page stateful so it can hold form key

--- flutter_app/lib/login_page/login_page.dart
+++ flutter_app/lib/login_page/login_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -16,28 +25,29 @@ class LoginPage extends StatelessWidget {
           backgroundColor: Colors.black,
         ),
         body: Form(
+            key: _formKey,
             child: Padding(
-          padding: const EdgeInsets.fromLTRB(60, 50, 60, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              TextFormField(
-                textAlign: TextAlign.center,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  labelText: "Username",
-                ),
+              padding: const EdgeInsets.fromLTRB(60, 50, 60, 0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      labelText: "Username",
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    autocorrect: false,
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: const InputDecoration(labelText: "Password"),
+                  )
+                ],
               ),
-              const SizedBox(height: 50),
-              TextFormField(
-                textAlign: TextAlign.center,
-                autocorrect: false,
-                obscureText: true,
-                obscuringCharacter: '*',
-                decoration: const InputDecoration(labelText: "Password"),
-              )
-            ],
-          ),
-        )));
+            )));
   }
 }
